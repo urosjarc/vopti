db2b607b8797211a91d15d18e094d1b4fe06b1c1
urosjarc <jar.fmf@gmail.com>
2024-04-13 10:52:25 +0200

    Adding graphics interface for drawing shapes.

--- src/main/kotlin/com/urosjarc/vopti/gui/windows/VOpti.kt
+++ src/main/kotlin/com/urosjarc/vopti/gui/windows/VOpti.kt
@@ -1,40 +1,31 @@
 package com.urosjarc.vopti.gui.windows
 
+import com.urosjarc.vopti.shared.Painter
 import javafx.fxml.FXML
-import javafx.scene.control.Tab
-import javafx.scene.control.TabPane
+import javafx.scene.layout.Pane
 import org.koin.core.component.KoinComponent
+
 
 abstract class VOptiUI : KoinComponent {
     @FXML
-    lateinit var tablesT: Tab
+    lateinit var downPane: Pane
 
     @FXML
-    lateinit var columnsT: Tab
-
-    @FXML
-    lateinit var connectionsT: Tab
-
-    @FXML
-    lateinit var schemasT: Tab
-
-    @FXML
-    lateinit var queriesT: Tab
-
-    @FXML
-    lateinit var tablesParentT: Tab
-
-    @FXML
-    lateinit var dbT: Tab
-
-    @FXML
-    lateinit var tablesTP: TabPane
-
-    @FXML
-    lateinit var commitsT: Tab
+    lateinit var upPane: Pane
 }
 
 class VOpti : VOptiUI() {
 
+    private val upPainter = Painter()
+    private val downPainter = Painter()
+
+    @FXML
+    fun initialize() {
+        this.upPainter.init(pane = this.upPane)
+        this.downPainter.init(pane = this.downPane)
+        this.upPainter.addCircle(0.5, 0.5)
+        this.downPainter.addSquare(0.5, 0.5)
+        this.downPainter.addSquareBox(0.5, 0.5, size = 1.0)
+    }
 
 }
---
+++ src/main/kotlin/com/urosjarc/vopti/shared/Vector.kt
@@ -0,0 +1,3 @@
+package com.urosjarc.vopti.shared
+
+data class Vector(var x: Double = 0.0, var y: Double = 0.0)
