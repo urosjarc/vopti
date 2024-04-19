79739d2988618ce9c36877d5aaa7f84893ec4b63
urosjarc <jar.fmf@gmail.com>
2024-04-19 08:42:12 +0200

    Creating functional interface.

--- core/src/main/kotlin/com/urosjarc/vopti/core/domain/CWSProblem.kt
+++ core/src/main/kotlin/com/urosjarc/vopti/core/domain/CWSProblem.kt
@@ -1,6 +1,7 @@
 package com.urosjarc.vopti.core.domain
 
 import java.time.Instant
+import kotlin.random.Random
 
 data class CWSProblem(
     override val id: Id<CWSProblem> = Id(),
@@ -24,6 +25,43 @@ data class CWSProblem(
 
     val created: Instant = Instant.now()
 ) : Entity<CWSProblem>() {
+
+    val customers: List<Location>
+    var depots: List<Location>
     val mapHeightFun = TestFunction.all.first { it.name == this.mapHeight }
     val customersDistributionFun = TestFunction.all.first { it.name == this.customersDistribution }
+
+    init {
+        val customerDistributionMap = this.customersDistributionFun.heightMap(resolution = this.mapResolution)
+        val heightMap = this.mapHeightFun.heightMap(resolution = this.mapResolution)
+
+        val depotRand = Random(seed = this.depotsSeed)
+        this.depots = arrayOfNulls<Location>(this.depotsSize).map {
+            val x = depotRand.nextDouble()
+            val y = depotRand.nextDouble()
+            val mapX = (x * this.mapResolution).toInt()
+            val mapY = (y * this.mapResolution).toInt()
+            val height = heightMap[mapY][mapX]
+            Location(x = x, y = y, height = height)
+        }
+
+        val customerRand = Random(seed = this.customersSeed)
+        this.customers = arrayOfNulls<Location>(this.customersSize).map {
+            val location: Location
+            while (true) {
+                val x = customerRand.nextDouble()
+                val y = customerRand.nextDouble()
+                val mapX = (x * this.mapResolution).toInt()
+                val mapY = (y * this.mapResolution).toInt()
+                val height = heightMap[mapY][mapX]
+                val probability = customerDistributionMap[mapY][mapX]
+                val random = customerRand.nextDouble()
+                if (random < probability - this.customersGrouping) {
+                    location = Location(x = x, y = y, height = height)
+                    break
+                }
+            }
+            location
+        }
+    }
 }
--- gui/src/main/kotlin/com/urosjarc/vopti/gui/widgets/CWSWidget.kt
+++ gui/src/main/kotlin/com/urosjarc/vopti/gui/widgets/CWSWidget.kt
@@ -3,16 +3,16 @@ package com.urosjarc.vopti.gui.widgets
 import com.urosjarc.vopti.app.algos.EuclidianCWS
 import com.urosjarc.vopti.core.algos.CWS
 import com.urosjarc.vopti.core.domain.CWSProblem
-import com.urosjarc.vopti.core.domain.Location
 import com.urosjarc.vopti.core.domain.Vector
+import com.urosjarc.vopti.gui.utils.Job
 import com.urosjarc.vopti.gui.utils.Painter
+import javafx.application.Platform
 import javafx.fxml.FXML
 import javafx.scene.control.Button
 import javafx.scene.layout.Pane
 import javafx.scene.paint.Color
 import org.apache.logging.log4j.kotlin.logger
 import org.koin.core.component.KoinComponent
-import kotlin.random.Random
 
 
 abstract class CWSWidgetUI : KoinComponent {
@@ -35,111 +35,101 @@ abstract class CWSWidgetUI : KoinComponent {
 }
 
 class CWSWidget : CWSWidgetUI() {
-    private val painter = Painter()
     private val log = this.logger()
-    private var problem: CWSProblem? = null
+
     private var cws: CWS? = null
+    private var problem: CWSProblem? = null
+
+    private val painter = Painter()
+
+    private var job: Thread? = null
 
     @FXML
     fun initialize() {
-        this.CWSProblem_Controller.problemCreated.listen { this.create(it) }
+        this.painter.init(pane = this.painterP)
+
+        this.CWSProblem_Controller.problemCreated.listen {
+            this.cws = null
+            this.problem = it
+            this.redraw()
+        }
         this.log.info(this)
         this.nextB.setOnAction { this.next() }
         this.solveB.setOnAction { this.solve() }
-        this.painter.init(pane = this.painterP)
         this.playB.setOnAction { this.play() }
+
+
         this.CWSProblem_Controller.create()
+    }
+
+    private fun initCWS() {
+        this.problem?.let { problem ->
+            this.cws = EuclidianCWS(
+                maxDistance = problem.vehicleRange,
+                depot = problem.depots.first(),
+                customers = problem.customers
+            )
+
+            this.cws!!.init()
+        }
     }
 
     fun redraw() {
         this.painter.clear()
-
-        this.problem?.let {
-            this.painter.heightMap(
-                testFunction = it.mapHeightFun,
-                resolution = it.mapResolution
-            )
-        }
-
-        this.cws?.let {
-            this.painter.addSquare(x = it.depot.x, y = it.depot.y, color = Color.RED, size = 0.005)
-            it.customers.forEach { loc -> this.painter.addCircle(x = loc.x, y = loc.y, fill = Color.GREEN, size = 0.005) }
-            it.routes.values.forEach { route ->
-                this.painter.addSquareBox()
+        this.cws?.let { cws ->
+            cws.routes.values.forEachIndexed { routeI, route ->
+                val routeColor = Color.hsb(360.0 / cws.routes.size * routeI, 1.0, 1.0)
                 for (i in 0 until route.vertices.size - 1) {
                     val curr = route.vertices[i] - 1
                     val next = route.vertices[i + 1] - 1
                     this.painter.addSolidLine(
-                        start = Vector(x = it.customers[curr].x, y = it.customers[curr].y),
-                        end = Vector(x = it.customers[next].x, y = it.customers[next].y),
+                        start = Vector(x = cws.customers[curr].x, y = cws.customers[curr].y),
+                        end = Vector(x = cws.customers[next].x, y = cws.customers[next].y),
+                        width = 0.001,
+                        color = routeColor
                     )
                 }
             }
         }
-
+        this.problem?.let { problem ->
+            this.painter.heightMap(
+                testFunction = problem.mapHeightFun,
+                resolution = problem.mapResolution
+            )
+            problem.depots.forEach { loc -> this.painter.addSquare(x = loc.x, y = loc.y, color = Color.RED, size = 0.005) }
+            problem.customers.forEach { loc -> this.painter.addCircle(x = loc.x, y = loc.y, fill = Color.GREEN, size = 0.005) }
+        }
         this.painter.redraw()
     }
 
-    private fun create(problem: CWSProblem) {
-        this.problem = problem
 
-        val customerDistributionMap = problem.customersDistributionFun.heightMap(resolution = problem.mapResolution)
-        val heightMap = problem.mapHeightFun.heightMap(resolution = problem.mapResolution)
-
-        val depotRand = Random(seed = problem.depotsSeed)
-        val depots = arrayOfNulls<Location>(problem.depotsSize).map {
-            val x = depotRand.nextDouble()
-            val y = depotRand.nextDouble()
-            val mapX = (x * problem.mapResolution).toInt()
-            val mapY = (y * problem.mapResolution).toInt()
-            val height = heightMap[mapY][mapX]
-            Location(x = x, y = y, height = height)
+    fun next(): Boolean {
+        this.cws ?: this.initCWS()
+        var count = 10e6
+        while (this.cws?.next() == false) {
+            count--
+            if (count < 0) return false
         }
-
-        val customerRand = Random(seed = problem.customersSeed)
-        val customers: List<Location> = arrayOfNulls<Location>(problem.customersSize).map {
-            val location: Location
-            while (true) {
-                val x = customerRand.nextDouble()
-                val y = customerRand.nextDouble()
-                val mapX = (x * problem.mapResolution).toInt()
-                val mapY = (y * problem.mapResolution).toInt()
-                val height = heightMap[mapY][mapX]
-                val probability = customerDistributionMap[mapY][mapX]
-                val random = customerRand.nextDouble()
-                if (random < probability - problem.customersGrouping) {
-                    location = Location(x = x, y = y, height = height)
-                    break
-                }
-            }
-            location
-        }
-
-        this.cws = EuclidianCWS(
-            maxDistance = problem.vehicleRange,
-            depot = depots.first(),
-            customers = customers
-        )
-        this.cws!!.init()
-
-        this.redraw()
-
-
-    }
-
-    fun next() {
-        this.cws?.next()
-        this.redraw()
+        Job.start { Platform.runLater { this.redraw() } }
+        return true
     }
 
     private fun solve() {
+        this.cws ?: this.initCWS()
         this.cws?.solve()
         this.redraw()
     }
 
     private fun play() {
-        println("play solve")
-    }
+        this.job?.interrupt()
+        this.cws ?: this.initCWS()
 
+        val interval = this.CWSProblem_Controller.solverPlayIntervalS.value.toLong()
+        this.job = Job.start(sleep = 0L, interval = interval, repeat = true) {
+            if (!this.next()) {
+                this.job?.interrupt()
+            }
+        }
+    }
 
 }
